baca2c8c837cf2c9d81f2f75ef148ffcbe222bc6
Nathan Wong <nathan@nathanwong.co.uk>
2020-02-23 21:32:10 +0000

    Initial Commit

---
+++ Bomb TV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bomb TV
+//
+//  Created by Nathan Wong on 23/02/2020.
+//  Copyright © 2020 Nathan Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bomb TVTests/Bomb_TVTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bomb_TVTests.swift
+//  Bomb TVTests
+//
+//  Created by Nathan Wong on 23/02/2020.
+//  Copyright © 2020 Nathan Wong. All rights reserved.
+//
+
+import XCTest
+@testable import Bomb_TV
+
+class Bomb_TVTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
